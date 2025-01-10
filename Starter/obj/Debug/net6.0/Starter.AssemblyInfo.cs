2237ed04a6c3539d58fc318c966fc2e4920ee9f3
Samuel Lima <samuelgomeslima@hotmail.com>
2025-01-09 23:53:13 -0300

    Debug changes

--- Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d72908c5b7eb4bb2aec18f4ce0f28507ce5bbe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
